fc72b54ec10fbae895621d7e10c1b47920199127
rishi <ashutoshrishi92@gmail.com>
2016-09-12 13:10:54 +1000

    Separated font locking code into its own module

---
+++ wybe-font-lock.el
@@ -0,0 +1,125 @@
+;;; wybe-mode --- Major mode for the wybe programming language
+
+;; Copyright 2016 Ashutosh Rishi Ranjan
+
+;; Author: Ashutosh Rishi Ranjan (ashutoshrishi92@gmail.com)
+;; Version: 1.0.0
+;; Created: 20 Feb 2015
+;; Keywords: languages
+
+;;; Commentary:
+;; Font locks for the Wybe Major mode to for writing Wybe modules.
+
+;;; Code:
+(require 'rx)
+(require 'font-lock)
+
+(defgroup wybe-font-lock nil
+  "Font locking for Wybe code."
+  :group 'faces)
+
+(defface wybe-font-lock-operators
+  '((t :inherit font-lock-builtin-face))
+  "The default face used to highlight operator functions/procs."
+  :group 'wybe-font-lock)
+
+(defcustom elm-font-lock-operators-face 'wybe-font-lock-operators
+  "The face used to highlight operators.
+To disable this highlighting, set to nil."
+  :type '(choice (const nil)
+                 face)
+  :group 'wybe-font-lock)
+
+;; Font locking for various syntactical features of Wybe
+
+;; Keywords
+
+(defconst wybe--keywords
+  '("func" "if" "then" "else" "proc" "end" "public" "private" "use"
+    "type" "do" "until" "unless" "or" "test" "import")
+  "Keywords of the Wybe language.")
+
+(defconst wybe--keywords-regexp
+  (concat (regexp-opt wybe--keywords 'words) "[^']")
+  "The regular expression matching Wybe keywords.")
+
+(defconst wybe--font-lock-keywords
+  (cons wybe--keywords-regexp font-lock-keyword-face)
+  "Highlighting for keywords.")
+
+;; Primitive Types and user defined types
+(defconst wybe--prim-types
+  '("int" "float" "string" "char" "bool")
+  "Wybe primitive types.")
+
+
+(defconst wybe--prim-types-regexp
+  (regexp-opt wybe--prim-types 'words)
+  "The regular expression matching certain primitive Wybe types.")
+
+(defconst wybe--custom-types-regexp
+  (rx (char ?:) (0+ space)
+      (group (1+ (any alnum))))
+  "Regex for custom type annotations.")
+
+(defconst wybe--font-lock-prim-types
+  (cons wybe--prim-types-regexp font-lock-type-face)
+  "Highlighting for primitive types.")
+
+(defconst wybe--font-lock-custom-types
+  (cons wybe--custom-types-regexp '(1 font-lock-type-face))
+  "Highlighting for custom Wybe types.")
+
+
+;; Functions and Procedure names
+
+(defconst wybe--func-regexp
+  (rx symbol-start (or "proc" "func" "type") (1+ space)
+      (group (1+ (any alnum ?+ ?- ?*)))
+      (any space ?\())
+  "The regular expression for matching function and proc names.")
+
+(defconst wybe--font-lock-func
+  (cons wybe--func-regexp font-lock-function-name-face)
+  "Highlighting for function and procedure names.")
+
+;; Operators
+
+(defconst wybe--operators-regexp
+  "[][;,()|{}]\\|[-@^!:*=<>&/%+~?#]"
+  "The regular expression for matching Wybe in-built operators.")
+
+(defconst wybe--font-lock-operators
+  (cons wybe--operators-regexp '(1 wybe-font-lock-operators-face))
+  "Highlighting for operator functions.")
+
+
+;; Syntax Table
+
+(defvar wybe--syntax-table
+  (let ((st (make-syntax-table)))
+    (modify-syntax-entry ?# "< b" st)
+    (modify-syntax-entry ?\n "> b" st)
+    st)
+  "Syntax table of the Wybe mode.")
+
+
+;; Final Highlighting
+
+(defconst wybe--font-lock-highlighting
+  (list (list wybe--font-lock-keywords
+              wybe--font-lock-prim-types
+              wybe--font-lock-func
+              wybe--font-lock-custom-types
+              wybe--font-lock-operators)
+        nil nil)
+  "Combined highlighting for various syntactical features of Wybe.")
+
+(defun turn-on-wybe-font-lock ()
+  "Turn on Wybe-mode font lock."
+  (setq font-lock-multiline t)
+  (set-syntax-table wybe--syntax-table)
+  (set (make-local-variable 'font-lock-defaults) wybe--font-lock-highlighting))
+
+(provide 'wybe-font-lock)
+;;; wybe-font-lock.el ends here
--- wybe-mode.el
+++ wybe-mode.el
@@ -11,97 +11,28 @@
 ;; Major mode to for writing Wybe modules.
 
 ;;; Code:
-
+(require 'wybe-font-lock)
 
 (defgroup wybe nil
   "Wybe Programming Language Major Mode."
   :group 'languages)
 
 
+;; Wybe mode hook
+(defvar wybe-mode-hook nil)
+
 ;; Wybe key map
 (defvar wybe-mode-map
   (let ((map (make-keymap)))
     (define-key map "\C-j" 'newline-and-indent)
     map)
-  "Keymap for Wybe major mode")
+  "Keymap for Wybe major mode.")
 
-
-;; Wybe faces
-(defgroup wybe-faces nil
-  "Custom faces for syntax highlighting."
-  :group 'wybe)
-
-(defface wybe-font-lock-operator-face
-  '((((background light)) (:foreground "brown"))
-    (t (:foreground "khaki")))
-  "Face description for all operators."
-  :group 'wybe-faces)
-(defvar wybe-font-lock-operator-face
-  'wybe-font-lock-operator-face)
-
-
-
-;; define several category of keywords
-(defconst wybe-keywords
-  '("func" "if" "then" "else" "proc" "end" "public" "private" "use"
-    "type" "do" "until" "unless" "or" "test")
-  "Keywords of the language.")
-
-(defconst wybe-types '("int" "float" "string" "char" "bool")
-  "Wybe primitive types.")
-
-
-;; Define REGEXs
-;; generate regex string for each category of keywords
-(defconst wybe-keywords-re (regexp-opt wybe-keywords 'words)
-  "Regex for matching Wybe keywords.")
-
-(defconst wybe-types-re (regexp-opt wybe-types 'words)
-  "Regex for matching Wybe primitive types.")
-
-(defconst wybe-func-re
-  (rx symbol-start (or "proc" "func" "type") (1+ space)
-      (group (1+ (any alnum ?+ ?- ?*)))
-      (any space ?\())
-  "Regex for matching function and proc names.")
-
-
-(defconst wybe-custom-types-re
-  (rx (char ?:) (0+ space)
-      (group (1+ (any alnum))))
-  "Regex for custom type annotations.")
-
-
-(defconst wybe-operators-re
-  "[][;,()|{}]\\|[-@^!:*=<>&/%+~?#]"
-  "Regex for matching Wybe in-built operators.")
-
-
-;; create the list for font-lock.
-;; each category of keyword is given a particular face
-(defvar wybe-font-lock-keywords
-  `((,wybe-keywords-re . font-lock-keyword-face)
-    (,wybe-types-re . font-lock-type-face)
-    (,wybe-operators-re . wybe-font-lock-operator-face)
-    (,wybe-func-re . (1 font-lock-function-name-face))
-    (,wybe-custom-types-re . (1 font-lock-type-face)))
-  "Wybe language font-locks.")
-
-
-;; Wybe syntax table
-(defvar wybe-syntax-table nil "Syntax table for `wybe-mode'.")
-(setq wybe-syntax-table
-      (let ((synTable (make-syntax-table c-mode-syntax-table)))
-
-        (modify-syntax-entry ?# "< b" synTable)
-        (modify-syntax-entry ?\n "> b" synTable)
-
-        synTable))
 
 
 ;; Indentation
 (defun wybe-indent-line ()
-  "Indent current line as Wybe code"
+  "Indent current line as Wybe code."
   (interactive)
   (let ((savep (> (current-column) (current-indentation)))
         (indent (condition-case nil (max (wybe-calculate-indentation) 0)
@@ -112,24 +43,46 @@
 
 (defun wybe-calculate-indentation ()
   "Return the column to which the current line should be indented."
-  (current-indentation))
+  (save-excursion
+    (beginning-of-line)
+    ;; Definite 0 column indentations
+    (if (looking-at "^[ \t]*\\(proc\\|test\\|public\\|private\\|type\\)")
+        0
+      ;; Check for block ending statements, then move back 4 spaces based
+      ;; on previous line indentation.
+      (if (looking-at "^[ \t]*end")
+          (progn
+            (forward-line -1)
+            (- (current-indentation) 4))
+        ;; Decide indentation by looking at the previous line(s)
+        (progn
+          ;; Move upwards until non-blank line
+          (forward-line -1)
+          (while (and (looking-at "[[:space:]]*$") (not (bobp)))
+            (forward-line -1))
+          ;; Heuristic based on previous line
+          (if (looking-at "[ \t]*\\(proc\\|if\\|type\\)")
+              (+ (current-indentation) 4)
+            (if (looking-at "^[ \t]*end")
+                (- (current-indentation) 4)
+              (current-indentation))))))))
+
 
 
 ;;;###autoload
 (define-derived-mode wybe-mode prog-mode
-  "wybe mode"
-  "Major mode for editing wybe language"
-
+  "Wybe"
+  "Major mode for editing Wybe modules."
+  (setq-default indent-tabs-mode nil)
   ;; code for syntax highlighting
-  (set-syntax-table wybe-syntax-table)
   (setq-local comment-start "#")
   (setq-local comment-end "")
-  (setq-local font-lock-defaults '((wybe-font-lock-keywords)))
-  ;; (setq-local indent-line-function 'wybe-indent-line)
-  )
+  (turn-on-wybe-font-lock)
+  (setq-local indent-line-function 'wybe-indent-line))
 
 ;;;###autoload
 (add-to-list 'auto-mode-alist '("\\.wybe\\'" . wybe-mode))
 
 (provide 'wybe-mode)
 ;;; wybe-mode.el ends here
+
